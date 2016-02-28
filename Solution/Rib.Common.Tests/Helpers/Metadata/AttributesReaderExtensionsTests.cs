48c59c8a812da46ed0fff8a8a0ee09b23cc5fb14
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-02-28 15:45:05 +0300

    AttributeReaderExtensions test

--- Solution/Rib.Common.Tests/Helpers/Metadata/AttributesReaderExtensionsTests.cs
+++ Solution/Rib.Common.Tests/Helpers/Metadata/AttributesReaderExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Rib.Common.Helpers.Metadata
 {
     using System;
+    using System.Linq;
     using Rib.Common.Models.Exceptions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
@@ -32,6 +33,33 @@
         {
             _attributesReader.Setup(x => x.ReadMany(typeof(DescriptionAttribute), typeof (string))).Returns(new  object[] { (DescriptionAttribute)null }).Verifiable();
             _attributesReader.Object.ReadSafe<DescriptionAttribute>(typeof (string));
+        }
+
+        [TestMethod]
+        public void ReadGenericTest()
+        {
+            _attributesReader.Setup(x => x.ReadMany(typeof(DescriptionAttribute), typeof(string))).Returns(new object[] { (DescriptionAttribute)null }).Verifiable();
+            Assert.IsNull(_attributesReader.Object.Read<DescriptionAttribute>(typeof(string)));
+        }
+
+        [TestMethod]
+        public void ReadTest()
+        {
+            var descriptionAttribute = new DescriptionAttribute("fad");
+            _attributesReader.Setup(x => x.ReadMany(typeof(DescriptionAttribute), typeof(string))).Returns(new object[] { descriptionAttribute  }).Verifiable();
+            Assert.AreEqual(descriptionAttribute, _attributesReader.Object.Read<DescriptionAttribute>(typeof(string)));
+        }
+
+        [TestMethod]
+        public void ReadManyTest()
+        {
+            var descriptionAttribute1 = new DescriptionAttribute("fad");
+            var descriptionAttribute2 = new DescriptionAttribute("fadawdawd");
+            _attributesReader.Setup(x => x.ReadMany(typeof(DescriptionAttribute), typeof(string))).Returns(new object[] { descriptionAttribute1, descriptionAttribute2 }).Verifiable();
+            var attrs = _attributesReader.Object.ReadMany<DescriptionAttribute>(typeof (string)).ToList();
+            Assert.AreEqual(2, attrs.Count);
+            Assert.AreEqual(descriptionAttribute1, attrs[0]);
+            Assert.AreEqual(descriptionAttribute2, attrs[1]);
         }
 
         [TestMethod]
