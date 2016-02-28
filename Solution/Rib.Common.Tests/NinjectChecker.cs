20a6a99f021a67ca39d6f8ac333c9602de1157ae
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-02-28 10:27:54 +0300

    Add ninject checker

---
+++ Solution/Rib.Common.Tests/NinjectChecker.cs
@@ -0,0 +1,44 @@
+﻿namespace Rib.Common
+{
+    using System.Linq;
+    using global::Ninject;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Rib.Common.Helpers.Cache;
+    using Rib.Common.Helpers.Configuration;
+    using Rib.Common.Helpers.Configuration.Services;
+    using Rib.Common.Helpers.Configuration.SettingsManagers;
+    using Rib.Common.Helpers.CorrelationId;
+    using Rib.Common.Helpers.Encrypting.Symmetric;
+    using Rib.Common.Helpers.Tmp;
+    using Rib.Common.Ninject;
+    using Rib.Tests.Ninject;
+
+    [TestClass]
+    public class NinjectCheckerTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var errors = new NinjectChecker(typeof (IResolver).Assembly, TestKernelCreator.Create(() => new StandardKernel(new RibCommonModule())))
+                .Exclude<ISymmetricAlgorithmTypeReader>()
+                .Exclude<ICorrelationIdStore>()
+                .Exclude<ISettingsManager>()
+                .Exclude<ISettingsReader>()
+                .Exclude(typeof(ISettingsReader<>))
+                .Exclude<ISettingsWriter>()
+                .Exclude<ISettingsWriterFactory>()
+                .Exclude<ISettingsReaderFactory>()
+                .Exclude<IConfigurationTypeResolver>()
+                .Exclude<ICanEditItemChecker>()
+                .Exclude<ITmpFolder>()
+                .Exclude(typeof(IConfigurationEntryCreator<>))
+                .Exclude(typeof(ICacher<>))
+                .CheckAllInterfacesCanCreate()
+                .Errors;
+            if (errors.Any())
+            {
+                Assert.Fail(string.Join("\r\n\r\n", errors.Select(x => x.Message)));
+            }
+        }
+    }
+}
