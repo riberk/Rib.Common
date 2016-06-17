884d6b8e90df954210ec958c7554ce33f8bf8605
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-06-17 13:01:30 +0300

    Clousure test

---
+++ Solution/Rib.Common.Tests/Helpers/Expressions/ClousureTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Rib.Common.Helpers.Expressions
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ClousureTests
+    {
+        [TestMethod]
+        public void ToClousureTest()
+        {
+            var res = Clousure.ToClousure(1);
+            Assert.AreEqual(ExpressionType.MemberAccess, res.NodeType);
+            var me = res as MemberExpression;
+            Assert.IsTrue((me.Member.MemberType & MemberTypes.Field) != 0);
+
+            var resVal = Expression.Lambda<Func<int>>(res).Compile()();
+            Assert.AreEqual(1, resVal);
+        }
+    }
+}
