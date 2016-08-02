3996063f6c407d9d9f721666f87098496f3dc20f
riberk <riber32@gmail.com>
2016-08-02 09:11:36 +0300

    build helpers

--- build.release.bat
+++ build.release.bat
@@ -1,4 +1,5 @@
 @ ECHO off
 cls
-"c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Build.proj /m /p:ConfigurationName=Release /p:VisualStudioVersion=14.0%*
+SET /p ApiKey=nuget api key:
+"c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Build.proj /m /p:ConfigurationName=Release /p:ApiKey=%ApiKey% /p:VisualStudioVersion=14.0%*
 pause
